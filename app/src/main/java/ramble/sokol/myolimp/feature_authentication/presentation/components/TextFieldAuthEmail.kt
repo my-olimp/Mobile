0af9962fb3a673af7997f5575485657ef72af2e9
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-08-07 20:48:30 +0500

    initial implementation login

---
+++ app/src/main/java/ramble/sokol/myolimp/feature_authentication/presentation/components/TextFieldAuthEmail.kt
@@ -0,0 +1,69 @@
+package ramble.sokol.myolimp.feature_authentication.presentation.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+import ramble.sokol.myolimp.R
+import ramble.sokol.myolimp.ui.theme.BlueStart
+import ramble.sokol.myolimp.ui.theme.ProfileEditPlaceholder
+import ramble.sokol.myolimp.ui.theme.White
+
+@Composable
+fun TextFieldAuthEmail (
+    previousData: String,
+    label: String,
+    isEnabled: Boolean = true,
+    onTextChanged: (String) -> Unit,
+) {
+
+    val textValue = remember {
+        mutableStateOf(previousData)
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        label = {
+            Row {
+                Text(
+                    text = label,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.regular)),
+                        fontWeight = FontWeight(400),
+                        color = ProfileEditPlaceholder,
+                        letterSpacing = 0.3.sp,
+                    )
+                )
+            }
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = BlueStart,
+            focusedLabelColor = BlueStart,
+            cursorColor = BlueStart,
+            backgroundColor = White
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+            onTextChanged(it)
+        },
+        enabled = isEnabled
+    )
+}
