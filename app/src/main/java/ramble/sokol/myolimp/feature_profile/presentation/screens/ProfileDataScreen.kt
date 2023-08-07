6de9bc37cfd0bdf6ea93ea7d6ab64b59344b9d7e
lkeey <leon062007@mail.ru>
2023-08-07 21:09:27 +0300

    custom text field with drop down

--- app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/screens/ProfileDataScreen.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/screens/ProfileDataScreen.kt
@@ -1,6 +1,5 @@
 package ramble.sokol.myolimp.feature_profile.presentation.screens
 
-import android.widget.Toast
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -12,8 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
@@ -29,7 +30,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -40,6 +42,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
@@ -56,6 +59,7 @@ import ramble.sokol.myolimp.feature_profile.presentation.sheets.EditContactsShee
 import ramble.sokol.myolimp.feature_profile.presentation.sheets.EditEducationSheet
 import ramble.sokol.myolimp.feature_profile.presentation.sheets.EditPersonalInfoSheet
 import ramble.sokol.myolimp.feature_profile.presentation.sheets.EditPhotoSheet
+import ramble.sokol.myolimp.feature_profile.utils.ProfileEvent
 import ramble.sokol.myolimp.ui.theme.BottomBarTheme
 import ramble.sokol.myolimp.ui.theme.GreyProfileAchivement
 import ramble.sokol.myolimp.ui.theme.White
@@ -87,11 +91,13 @@ fun ProfileDataScreen(
         mutableStateOf("")
     }
 
+    var selectedImgUri by remember {
+        mutableStateOf(viewModel.state.value.profileImg)
+    }
+
     BottomBarTheme(
         navController = navController
     ) {
-
-        val context = LocalContext.current
 
         Column(
             modifier = Modifier
@@ -116,10 +122,15 @@ fun ProfileDataScreen(
                 verticalArrangement = Arrangement.Center
             ) {
 
-                Image(
-                    modifier = Modifier.padding(top = 8.dp),
-                    painter = painterResource(id = R.drawable.ic_default_img),
-                    contentDescription = "user image"
+                AsyncImage(
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .size(95.dp)
+                        .align(CenterHorizontally)
+                        .clip(CircleShape),
+                    model = if (selectedImgUri != null) selectedImgUri else R.drawable.ic_default_img,
+                    contentDescription = "user logo",
+                    contentScale = ContentScale.Crop
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -133,7 +144,8 @@ fun ProfileDataScreen(
                     }
 
                     ProfileOutlinedBtn(text = stringResource(R.string.delete)) {
-                        Toast.makeText(context, "It's developing", Toast.LENGTH_SHORT).show()
+                        selectedImgUri = null
+                        viewModel.onEvent(ProfileEvent.OnImgDelete)
                     }
                 }
 
--- app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/screens/ProfileScreen.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/screens/ProfileScreen.kt
@@ -3,7 +3,6 @@ package ramble.sokol.myolimp.feature_profile.presentation.screens
 import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,16 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -31,6 +33,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.navigate
 import org.koin.androidx.compose.getViewModel
@@ -49,11 +52,13 @@ import ramble.sokol.myolimp.ui.theme.White
 fun ProfileScreen(
     navController: NavController
 ) {
+
+    val viewModel = getViewModel<ProfileViewModel>()
+    val context = LocalContext.current
+
     BottomBarTheme(
         navController = navController
     ) {
-
-        val context = LocalContext.current
 
         Column(
             modifier = Modifier
@@ -79,11 +84,17 @@ fun ProfileScreen(
                 verticalArrangement = Arrangement.Center
             ) {
 
-                Image(
-                    modifier = Modifier.padding(top = 8.dp),
-                    painter = painterResource(id = R.drawable.ic_default_img),
-                    contentDescription = "user image"
+                AsyncImage(
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .size(95.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .clip(CircleShape),
+                    model = if (viewModel.state.value.profileImg != null) viewModel.state.value.profileImg else R.drawable.ic_default_img,
+                    contentDescription = "user logo",
+                    contentScale = ContentScale.Crop
                 )
+
 
                 Spacer(modifier = Modifier.height(8.dp))
 
