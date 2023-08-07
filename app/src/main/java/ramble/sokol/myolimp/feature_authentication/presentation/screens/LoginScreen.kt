9f935f155c84995e2e4addf072d558131373b373
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-08-07 15:16:40 +0500

    add change lera

--- app/src/main/java/ramble/sokol/myolimp/feature_authentication/presentation/screens/LoginScreen.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_authentication/presentation/screens/LoginScreen.kt
@@ -1,17 +1,64 @@
 package ramble.sokol.myolimp.feature_authentication.presentation.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import ramble.sokol.myolimp.R
+import ramble.sokol.myolimp.ui.theme.BlackProfile
 import ramble.sokol.myolimp.ui.theme.OlimpTheme
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Destination
 @Composable
 fun LoginScreen (
     navigator: DestinationsNavigator
 ) {
     OlimpTheme {
-        Text(text = "LoginScreen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 24.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.auth_my_olimp),
+                contentDescription = "image auth my olimp"
+            )
+
+            Spacer(modifier = Modifier.padding(top = 9.dp))
+
+            Text(
+
+                text = stringResource(id = R.string.login_to_service),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.regular)),
+                    fontWeight = FontWeight(400),
+                    color = BlackProfile,
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
     }
 }
