fc3637f1811c91cde74cef954ddc0341dea2eb1f
lkeey <leon062007@mail.ru>
2023-08-19 14:24:22 +0300

    add ability to check favourites

--- app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_favourites/Favourites.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_favourites/Favourites.kt
@@ -1,11 +1,11 @@
 package ramble.sokol.myolimp.feature_calendar.presentation.components.feature_favourites
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
 import ramble.sokol.myolimp.R
 import ramble.sokol.myolimp.destinations.UpdateScreenDestination
-import ramble.sokol.myolimp.feature_calendar.domain.events.Event
 import ramble.sokol.myolimp.feature_calendar.domain.states.PlanState
 import ramble.sokol.myolimp.feature_calendar.presentation.components.feature_create.ImageWithText
 import ramble.sokol.myolimp.feature_calendar.presentation.components.feature_current_day.PlanItem
@@ -15,16 +15,16 @@ fun Favourites (
     state: PlanState,
     navController: NavController,
 ) {
+
     val favouritesPlans = state.plans.filter {
-        it.subject.contains(state.searchQuery, ignoreCase = true) ||
-                it.title.contains(state.searchQuery, ignoreCase = true)
+        it.isFavourite
     }
 
     if (favouritesPlans.isEmpty()) {
 
         ImageWithText (
             drawable = R.drawable.ic_calendar_no_plans,
-            text = "Здесь пока пусто :("
+            text = stringResource(R.string.no_favourites)
         )
 
     } else {
