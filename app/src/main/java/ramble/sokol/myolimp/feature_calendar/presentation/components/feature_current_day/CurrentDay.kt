8ea9cfd35f2ff2b99c6b548ec61fc45f8fe85e5e
lkeey <leon062007@mail.ru>
2023-08-20 22:04:08 +0300

    update current day view

--- app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_current_day/CurrentDay.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_current_day/CurrentDay.kt
@@ -1,14 +1,34 @@
 package ramble.sokol.myolimp.feature_calendar.presentation.components.feature_current_day
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
 import ramble.sokol.myolimp.R
 import ramble.sokol.myolimp.destinations.UpdateScreenDestination
 import ramble.sokol.myolimp.feature_calendar.domain.states.PlanState
+import ramble.sokol.myolimp.feature_calendar.domain.view_models.PlansViewModel
 import ramble.sokol.myolimp.feature_calendar.presentation.components.feature_create.ImageWithText
+import ramble.sokol.myolimp.ui.theme.GreyProfileAchivement
 import java.time.LocalDate
+import java.util.Calendar
 
 @Composable
 fun CurrentDay (
@@ -40,15 +60,118 @@ fun CurrentDay (
         )
 
     } else {
-        currentPlans.forEach {
-            PlanItem(
-                item = it,
-            ) { plan->
-                navController.navigate(
-                    UpdateScreenDestination(plan = plan)
-                )
+
+        // today
+        if (currentDate.value == LocalDate.now()) {
+
+            val rightNow = Calendar.getInstance()
+            val currentHour: Int = rightNow.get(Calendar.HOUR_OF_DAY) // 0..23
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+
+                val nextPlans = currentPlans
+                    .filter {
+
+                        Log.i(
+                            PlansViewModel.TAG,
+                            "$currentHour - ${it.startHour} - ${it.startHour < currentHour}"
+                        )
+
+                        it.startHour > currentHour
+                    }
+
+                if (nextPlans.isNotEmpty()) {
+
+                    CommentText(
+                        text = "Предстоящие события"
+                    )
+
+                    nextPlans.forEach {
+                        PlanItem(
+                            item = it,
+                        ) { plan ->
+                            navController.navigate(
+                                UpdateScreenDestination(plan = plan)
+                            )
+                        }
+                    }
+                }
+
+
+                val previousPlans = currentPlans
+                    .filter {
+                        Log.i(
+                            PlansViewModel.TAG,
+                            "$currentHour - ${it.endHour} - ${it.endHour < currentHour}"
+                        )
+
+                        it.endHour < currentHour
+                    }
+
+                if (previousPlans.isNotEmpty()) {
+
+                    CommentText(
+                        text = "Прошедшие события"
+                    )
+
+                    previousPlans.forEach {
+                        PlanItem(
+                            item = it,
+                        ) { plan ->
+                            navController.navigate(
+                                UpdateScreenDestination(plan = plan)
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(24.dp))
+
+//            currentPlans.forEach {
+//                PlanItem(
+//                    item = it,
+//                ) { plan->
+//                    navController.navigate(
+//                        UpdateScreenDestination(plan = plan)
+//                    )
+//                }
+//            }
+            }
+
+            // before today
+        } else if (currentDate.value.isBefore(LocalDate.now())) {
+
+            CommentText(
+                text = "Прошедшие события"
+            )
+
+            currentPlans.forEach {
+                PlanItem(
+                    item = it,
+                ) { plan ->
+                    navController.navigate(
+                        UpdateScreenDestination(plan = plan)
+                    )
+                }
+            }
+        // after
+
+        } else {
+            CommentText(
+                text = "Предстоящие события"
+            )
+
+            currentPlans.forEach {
+                PlanItem(
+                    item = it,
+                ) { plan ->
+                    navController.navigate(
+                        UpdateScreenDestination(plan = plan)
+                    )
+                }
             }
         }
     }
-
 }
