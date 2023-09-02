5081787d75d23a9825b971cd08b8b8418ad1b904
y19th <destounchannel@gmail.com>
2023-09-02 21:12:49 +0300

    feature/register-resonal AD-19

--- app/src/main/java/ramble/sokol/myolimp/feature_authentication/domain/states/RegistationInfoState.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_authentication/domain/states/RegistationInfoState.kt
@@ -2,7 +2,7 @@ package ramble.sokol.myolimp.feature_authentication.domain.states
 
 data class RegistrationInfoState(
     val fio: String = "",
-    val gender: String = "",
+    val gender: String = "f",
     val bdate: String = "",
     val activityType: String = ""
 )
