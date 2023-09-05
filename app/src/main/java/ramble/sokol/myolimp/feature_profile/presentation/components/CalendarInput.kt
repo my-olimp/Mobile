e94c124167218b481476b1283b3cedf7fc565274
lkeey <leon062007@mail.ru>
2023-09-05 20:25:43 +0300

    update calendar

--- app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/components/CalendarInput.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_profile/presentation/components/CalendarInput.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -113,7 +114,9 @@ fun CalendarInput(
             focusedLabelColor = BlueStart,
             cursorColor = BlueStart,
             backgroundColor = White,
-            errorBorderColor = MessageError
+            errorBorderColor = MessageError,
+            disabledTextColor = Color.Black,
+            disabledTrailingIconColor = Color.Black
         ),
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Next
