d49a35276870aea09807240621f167b5aca03d73
lkeey <leon062007@mail.ru>
2023-09-10 13:04:18 +0300

    fix the bug with calendar

--- app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_calendar/InlineCalendar.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_calendar/presentation/components/feature_calendar/InlineCalendar.kt
@@ -1,19 +1,20 @@
 package ramble.sokol.myolimp.feature_calendar.presentation.components.feature_calendar
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import ramble.sokol.myolimp.feature_calendar.domain.states.PlanState
 import ramble.sokol.myolimp.feature_calendar.domain.utils.dayViewModifier
 import java.time.LocalDate
 import java.time.YearMonth
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 internal fun InlineCalendar(
     loadedDates: Array<List<LocalDate>>,
@@ -29,32 +30,35 @@ internal fun InlineCalendar(
         it shows when calendar is collapsed
     */
 
-    val itemWidth = LocalConfiguration.current.screenWidthDp / 7
+//    val itemWidth = LocalConfiguration.current.screenWidthDp / 7
 
     CalendarPager(
         loadedDates = loadedDates,
         loadNextDates = loadNextWeek,
         loadPrevDates = loadPrevWeek
     ) { currentPage ->
-        Row {
-            loadedDates[currentPage]
-                .forEach { date ->
-                    Box(
-                        modifier = Modifier
-                            .width(itemWidth.dp)
-                            .padding(5.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
 
-                        DayView(
-                            date,
-                            isSelected = selectedDate == date,
-                            onDayClick = onDayClick,
-                            modifier = Modifier.dayViewModifier(date, currentMonth),
-                            state = state,
-                        )
-                    }
+        FlowRow (
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            loadedDates[currentPage].forEach { date ->
+                Box(
+                    modifier = Modifier
+//                            .width(itemWidth.dp)
+                        .fillMaxWidth(0.14f)
+                        .padding(5.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    DayView(
+                        date,
+                        isSelected = selectedDate == date,
+                        onDayClick = onDayClick,
+                        modifier = Modifier.dayViewModifier(date, currentMonth),
+                        state = state,
+                    )
                 }
+            }
         }
     }
 }
