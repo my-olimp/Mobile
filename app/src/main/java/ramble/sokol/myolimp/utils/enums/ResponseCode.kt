ef2b64193ee1fe573365c3f04352e1353cc3558e
y19th <whenmistdisappear@gmail.com>
2023-10-03 21:57:34 +0300

    add some response codes

--- app/src/main/java/ramble/sokol/myolimp/utils/enums/ResponseCode.kt
+++ app/src/main/java/ramble/sokol/myolimp/utils/enums/ResponseCode.kt
@@ -1,6 +1,11 @@
-package ramble.sokol.myolimp.utils.enums
+@file:Suppress("unused")package ramble.sokol.myolimp.utils.enums
 
 enum class ResponseCode(val code: Int) {
     SUCCESS(200),
+    BAD_REQUEST(400),
+    FORBIDDEN(403),
+    NOT_FOUND(404),
+    METHOD_NOT_ALLOWED(405),
+    TIMEOUT(408)
     /*TODO add some codes*/
 }
