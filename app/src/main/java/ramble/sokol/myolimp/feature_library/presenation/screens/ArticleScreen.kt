afc87568b0a34c8b429685213f4b77b73cc179cb
y19th <whenmistdisappear@gmail.com>
2023-10-17 13:58:53 +0300

    state commit

--- app/src/main/java/ramble/sokol/myolimp/feature_library/presenation/screens/ArticleScreen.kt
+++ app/src/main/java/ramble/sokol/myolimp/feature_library/presenation/screens/ArticleScreen.kt
@@ -28,16 +28,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.navigate
-import dev.jeziellago.compose.markdowntext.MarkdownText
 import org.koin.androidx.compose.getViewModel
 import ramble.sokol.myolimp.R
 import ramble.sokol.myolimp.destinations.ArticleScreenDestination
@@ -48,10 +42,8 @@ import ramble.sokol.myolimp.feature_library.presenation.components.article.PartI
 import ramble.sokol.myolimp.feature_library.presenation.components.article.SearchTextField
 import ramble.sokol.myolimp.feature_library.presenation.components.library.FavoriteIcon
 import ramble.sokol.myolimp.ui.theme.BackgroundMain
-import ramble.sokol.myolimp.ui.theme.BlueStart
 import ramble.sokol.myolimp.ui.theme.GreyProfileData
 import ramble.sokol.myolimp.ui.theme.OlimpTheme
-import ramble.sokol.myolimp.ui.theme.SheetTitle
 import ramble.sokol.myolimp.ui.theme.White
 
 
@@ -198,119 +190,5 @@ fun ArticleScreen(
 
             }
         }
-    )
-}
-
-
-//@Preview
-@Composable
-fun CustomMarkDown() {
-
-    val text = """
-# Алгоритм Дейкстры
-
-> *Программирование калечит мозг, поэтому обучение ему должно трактоваться как преступление.*
-Эдсгер Вибе Дейкстра
-> 
-
-**Алгоритм Дейкстры** — алгоритм нахождения кратчайших путей от данной вершины s до всех остальных во взешенном графе (V,E) с неотрицательными весами.
-
-**Асимптотика**: 
-
-$$|E| ≈ |V|^2$$
-$$|a=b+c|$$
-
-$$
-O(\,|E| · log(\,|V |\,)\,) - реализация \; для \; разреженных \;  графов
-$$
-
-## Постановка задачи
-
-Пусть дан взвешенный граф *(V, E)* с неотрицательными весами и вершина s. Необходимо найти кратчайшие пути от s до всех остальных вершин.
-Удобнее всего представлять граф либо в виде матрицы смежности (где отсутствие ребра будет заменяться ребром длины *INF*), либо в виде списка смежности. В статье будет использоваться именно список.
-Список смежности будет выглядеть как вектор векторов пар, где первый элемент в паре обозначает, куда ведёт ребро, а второй — его стоимость.
-
-## Алгоритм
-
-Заведём массив *d* длины *|V|*, где *d[i]* будет равен кратчайшему расстоянию от *s* до *i*. Изначально *d* заполнен *INF*, а *d[s] = 0*.
-Затем запустим цикл, который будет пытаться оптимизировать расстояние. Каждый раз он будет искать ближайшую необработанную вершину *i* и пытаться укоротить расстояние до его соседей. То есть для ребра *j:* 
-
-$$
-d [j.first] = min (d[j.first], d[i] + d[j.second]).
-$$
-
-Искать ближайшую вершину можно либо простым перебором, либо ис- пользуя такие типы данных как *set* и *priority_queue*. Проблема последнего способа заключается в том, что их обновление происходит за *O(log(N))*, и если это происходит часто, то скорость сильно теряется. Поэтому, если граф плотный, то есть
-
-$$
-|E| ≈ |V|^2
-$$
-
-то лучше использовать базовую реализацию, иначе через *set*.
-
-## Базовая реализация (C++)
-
-```cpp
-int N=V.size(); 
-vector <int> d(N, INF);
-d[s] = 0;
-vector <bool> visited (N, false ); 
-while (1) {
-	int mn = INF, id = −1;
-	for (int i = 0; i <N; ++i) if (!visited[i] &&d[i] <mn) {
-		mn = d [ i ] ; 
-		id = i;
-	}
-	if (id == −1) break;
-	visited[id] = true; 
-	for (auto i: V[id])
-		if (!visited[i.first] && d[i.first] > d[id] + d[i.second])
-			 d[i.first] = d[id] + d[i.second];
-}
-```
-
-## Восстановление путей
-
-Чтобы восстановить кратчайшие пути, нужно хранить в массиве p предыдущие вершины.
-
-```cpp
-int N=V.size();
-vector <int> d(N, INF), p(N, −1);
-d[s] = 0;
-vector <bool> visited (N, false ); 
-while (1) {
-	int mn = INF, id = −1;
-	for (int i = 0; i <N; ++i) if (!visited[i] &&d[i] <mn) {
-			mn = d [ i ]; 
-			id = i;
-}
-if (id == −1) break;
-visited[id] = true;
-for (auto i: V[id])
-	if (!visited[i.first] && d[i.first] > d[id] + d[i.second]) {
-		d[i.first] = d[id] + d[i.second];
-		p[i.first] = id;
-}
-```
-
-## Модификация для разреженных графов
-
-Если в графе относительно немного рёбер, то кратчайшие расстояния можно дублировать в *set ds*, чтобы быстро *(O(log(N))* вместо *O(N)* получать ближайшую необработанную вершину. При этом обновление расстояния будет происходить медленно: *O (log(N))* вместо *O(1)*
-    """
-
-    MarkdownText(
-        modifier = Modifier
-            .padding(16.dp),
-        markdown = text,
-        linkColor = BlueStart,
-        isTextSelectable = true,
-        style = androidx.compose.ui.text.TextStyle(
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            fontFamily = FontFamily(Font(R.font.medium)),
-            fontWeight = FontWeight(400),
-            color = SheetTitle,
-            letterSpacing = 0.22.sp,
-            textAlign = TextAlign.Start
-        ),
     )
 }
